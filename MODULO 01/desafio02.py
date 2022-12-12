fe02f95bc015f3d5f64251ffca79123f844bb64b
Jailson Souza Carvalho <78353254+JailsonSouza@users.noreply.github.com>
2022-12-12 00:26:54 -0300

    first comit

---
+++ MODULO 01/desafio02.py
@@ -0,0 +1,8 @@
+# desafio 02
+# Crie um script Python que leia o dia, mes e ano de nascimento
+# de uma pessoa e mostre uma mensagem com a data formatada
+print('=========== DESAFIO 02 ===========')
+dia = input('Dia = ')
+mes = input('Mes = ')
+ano = input('Ano = ')
+print(f'Você nasceu no dia {dia} de {mes} de {ano}. Correto?')
---
+++ MODULO 02/ex01.py
@@ -0,0 +1,2 @@
+print('Hello World!')
+print('Seja bem vindo ao seu curso completo de Python do Estudonauta!')
