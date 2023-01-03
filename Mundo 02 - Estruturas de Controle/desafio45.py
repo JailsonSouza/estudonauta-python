fa728e1ee467ba33ca6d699d3a96be13618a6c27
Jailson Souza Carvalho <78353254+JailsonSouza@users.noreply.github.com>
2023-01-03 00:01:40 -0300

    .

--- Mundo 02 - Estruturas de Controle/desafio45.py
+++ Mundo 02 - Estruturas de Controle/desafio45.py
@@ -3,6 +3,7 @@
 '''
 print(f'=========== DESAFIO 45 ===========')
 from random import randint
+from time import sleep
 
 opcoes = ['Pedra', 'Papel', 'Tesoura']
 comp = randint(0, 2)
@@ -13,19 +14,37 @@ print('''Suas escolhas
 [ 2 ] TESOURA''')
 user = int(input('Informe sua escolha? '))
 
+if (user != 0 and user != 1 and user != 2):
+    print('Jogada informada inválida!!!')
+else:  
+    print('JO')
+    sleep(1)
+    print('KEN')
+    sleep(1)
+    print('PÔ!!!')
+    sleep(1)
+    print('-=' * 20)
+    print(f'O computador jogou {opcoes[comp]}')
+    print(f'O jogador jogou {opcoes[user]}')
+    print('-=' * 20)
 
-print(f'O computador escolheu {opcoes[comp]}')
-print(f'O jogador escolheu {opcoes[user]}')
-
-if (user == 0 and comp == 1):
-    print('Computador ganhou')
-elif (user == 0 and comp == 2):
-    print('Usuario ganhou')
-elif (user == 1 and comp == 0):
-    print('Usuario ganhou')
-elif (user == 1 and comp == 2):
-    print('Computador ganhou')
-elif (user == 2 and comp == 0):
-    print('Computador ganhou')
-elif (user == 2 and comp == 1):
-    print('Usuario ganhou')
+    if (user == 0 and comp == 1):
+        print('COMPUTADOR GANHOU')
+    elif (user == 0 and comp == 2):
+        print('JOGADOR GANHOU')
+    elif (user == 1 and comp == 0):
+        print('JOGADOR GANHOU')
+    elif (user == 1 and comp == 2):
+        print('COMPUTADOR GANHOU')
+    elif (user == 2 and comp == 0):
+        print('COMPUTADOR GANHOU')
+    elif (user == 2 and comp == 1):
+        print('JOGADOR GANHOU')
+    elif (user == 0 and comp == 0):
+        print('EMPATE!!!')
+    elif (user == 1 and comp == 1):
+        print('EMPATE!!!')
+    elif (user == 2 and comp == 2):
+        print('EMPATE!!!')
+    else:
+        print('Escolha informada inválida!!!')
