2c2a0ba3a471df9ef96c0123845afd261b421868
Jailson Souza Carvalho <78353254+JailsonSouza@users.noreply.github.com>
2023-02-26 13:44:48 -0300

    .

--- Mundo 02 - Estruturas de Controle/desafio58.py
+++ Mundo 02 - Estruturas de Controle/desafio58.py
@@ -4,4 +4,22 @@
     mostrando no final quantos palpites foram necessários para vencer. 
 '''
 print('=========== DESAFIO 58 ===========')
+print('-=' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=' * 20)
 
+from random import randint
+from time import sleep
+
+qtd_palpites = 0
+comp = randint(0, 5)
+while (True):
+    user = int(input('Em que número eu pensei? '))
+    qtd_palpites += 1
+    if (user == comp):
+        break
+    print('PROCESSANDO...')
+    sleep(2)
+
+if (user == comp):
+    print(f'Ao todo foram nescessário {qtd_palpites} para você conseguir acertar o número que eu pensei!')
--- Mundo 02 - Estruturas de Controle/desafio59.py
+++ Mundo 02 - Estruturas de Controle/desafio59.py
@@ -9,4 +9,24 @@
     Seu programa deverá realizar a operação solicitada em cada caso.
 '''
 print('=========== DESAFIO 59 ===========')
+opc = 0
+
+n1 = int(input('Informe o primeiro número? '))
+n2 = int(input('Informe o segundo número? '))
+
+while (opc != 5):
+
+    print('''
+    ------------ TABUADA ------------
+    [1] SOMAR
+    [2] MULTIPLICAR
+    [3] MAIOR
+    [4] NOVOS NÚMEROS
+    [5] SAIR DO PROGRAMA 
+    ''')
+    opc = int(input('Informe a sua escolha? '))
+
+    if (opc == 1): # SOMAR
+        print(f'{n1} + {n2} = {n1+n2}')
+
 
